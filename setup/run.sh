284bd14c8df43749420ffd1b8510c117a6e0cee1
Luka Milic <luka.milic@ocado.com>
2020-01-03 15:49:57 +0000

    add zlib and tidy up run.sh

--- setup/run.sh
+++ setup/run.sh
@@ -2,29 +2,53 @@
 # For terminals
 sudo apt-get install zsh wget git curl -y
 sudo apt-get update -y; sudo apt-get upgrade -y
-sudo apt-get install -f
+
+#zsh
 chsh -s /bin/zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 sudo apt-get autoremove
 # For GUIs
 sudo apt-get install terminator gnome-tweak-tool \
                      gnome-shell-extensions chrome-gnome-shell \
-                     emacs
+                     emacs x2x
+# numix
 sudo add-apt-repository ppa:numix/ppa
 sudo apt install numix-icon-theme-circle numix-icon-theme numix-gtk-theme
 sudo apt-get update; sudo apt-get upgrade;
 sudo apt-get install -f
+
+# aliases
 echo 'source ~/dotfiles/.aliasrc' >> ~/.zshrc
+
+#terminator
 sudo update-alternatives --config x-terminal-emulator
+
+#pyenv
+sudo apt-get install zlib1g-dev
 curl https://pyenv.run | bash
+
+#rescuetime
 wget https://www.rescuetime.com/installers/rescuetime_current_amd64.deb -O ~/Downloads/rescuetime.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb
 sudo dpkg -i ~/Downloads/rescuetime.deb
+
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb
 sudo dpkg -i ~/Downloads/chrome-deb
-sudo apt-get install -f
+
+sudo apt-get install -f; sudo apt-get autoremove
+
+# snap
 sudo snap install spotify
-sudo snap install atom
-sudo apt-get autoremove
+sudo snap install atom --classic
+sudo snap install pycharm-professional --classic
+sudo snap install code --classic
+
+# git
 git config --global user.name "Luka Milic"
+
+#folders
+mkdir ~/Projects
